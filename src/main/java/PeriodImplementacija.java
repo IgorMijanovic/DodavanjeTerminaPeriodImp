f39c84688d73a3dadb08253ea39de2d2faeada9d
AleksaNikolic102 <anikolic10222rn@raf.rs>
2023-11-07 18:25:36 +0100

    aki

---
+++ src/main/java/PeriodImplementacija.java
@@ -0,0 +1,24 @@
+public class PeriodImplementacija extends ObradaTermina{
+
+
+    @Override
+    public boolean dodajNoviTermin() {
+        return false;
+    }
+
+    @Override
+    public boolean brisanjeTermina() {
+        return false;
+    }
+
+    @Override
+    public boolean premestanjeTermina() {
+        return false;
+    }
+
+    @Override
+    public boolean pretragaTermina() {
+        return false;
+    }
+
+}
