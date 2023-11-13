ae23ba0c3097fa17adff40e0004c5aef36e4ef8c
AleksaNikolic102 <anikolic10222rn@raf.rs>
2023-11-13 02:12:57 +0100

    skoro gotovo dodavanje i skroz gotovo brisanje

--- src/main/java/PeriodImplementacija.java
+++ src/main/java/PeriodImplementacija.java
@@ -101,7 +101,33 @@ public class PeriodImplementacija extends ObradaTermina{
             //System.out.println(dodaci);
         }
 
+        if(pocetakDan.getYear() == krajDan.getYear() && pocetakDan.getMonth() == krajDan.getMonth() && pocetakDan.getDayOfMonth()==krajDan.getDayOfMonth()){
+            if(pocetakDan.getDayOfWeek() == danEng){
+                LocalDateTime krajj = LocalDateTime.of(pocetakDan.getYear(),pocetakDan.getMonth(),pocetakDan.getDayOfMonth(),krajSati,0);
+                LocalDateTime pocetakk = LocalDateTime.of(pocetakDan.getYear(),pocetakDan.getMonth(),pocetakDan.getDayOfMonth(),pocetakSati,0);
+
+                //System.out.println(pocetakk);
+                //System.out.println(krajj);
+                Termin termin = new Termin(null,pocetakk,krajj,dodaci);
+                //System.out.println(termin);
+                int flag = 1;
+                List<Termin> provera = getRaspored();
+                for (Termin t: provera){
+                    if (zauzet(termin,t)){
+                        flag = 0;
+                        System.out.println("Ovaj termin nije dodat jer se preklapa sa drugim terminom" + " " + termin);
+                    }
+                }
+                if (flag == 1 || getRaspored().isEmpty()) {
+                    List<Termin> ddd = getRaspored();
+                    ddd.add(termin);
+                }
+
+            }
+        }
+
         for (LocalDateTime datumm = pocetakDan; datumm.isBefore(krajDan); datumm = datumm.plusDays(1)){
+
                 if(datumm.getDayOfWeek() == danEng){
                     LocalDateTime krajj = LocalDateTime.of(datumm.getYear(),datumm.getMonth(),datumm.getDayOfMonth(),krajSati,0);
                     LocalDateTime pocetakk = LocalDateTime.of(datumm.getYear(),datumm.getMonth(),datumm.getDayOfMonth(),pocetakSati,0);
@@ -109,8 +135,20 @@ public class PeriodImplementacija extends ObradaTermina{
                     //System.out.println(pocetakk);
                     //System.out.println(krajj);
                     Termin termin = new Termin(null,pocetakk,krajj,dodaci);
-                    List<Termin> ddd = getRaspored();
-                    ddd.add(termin);
+                    //System.out.println(termin);
+                    int flag = 1;
+                    List<Termin> provera = getRaspored();
+                        for (Termin t: provera){
+                            if (zauzet(termin,t)){
+                                flag = 0;
+                                System.out.println("Ovaj termin nije dodat jer se preklapa sa drugim terminom");
+                            }
+                        }
+                        if (flag == 1 || getRaspored().isEmpty()) {
+                            List<Termin> ddd = getRaspored();
+                            ddd.add(termin);
+                        }
+
                 }
 
         }
@@ -126,19 +164,225 @@ public class PeriodImplementacija extends ObradaTermina{
 
 
             List<Termin> ispis = getRaspored();
+        //System.out.println(ispis.size());
         for (Termin t : ispis){
-            System.out.println(t);
+            //System.out.println(t);
         }
         //dodaci.clear();
         return true;
     }
 
+    public boolean zauzet(Termin jedan,Termin dva){
+        if (jedan.getPocetak().getYear()==dva.getPocetak().getYear() && jedan.getPocetak().getMonth()==dva.getPocetak().getMonth() && jedan.getPocetak().getDayOfMonth()==dva.getPocetak().getDayOfMonth()){
+            if (jedan.getPocetak().getHour() >= dva.getKraj().getHour() || jedan.getKraj().getHour() <= dva.getPocetak().getHour()){
+               // System.out.println("Slobodan");
+                return false;
+            }else {
+               // System.out.println("Zauzetttttttttttt");
+                return true;
+            }
+        }else {
+            //System.out.println("zauzet");
+            return false;
+        }
 
+    }
 
 
     @Override
     public boolean brisanjeTermina(String... strings) {
-        return false;
+        List<String> stringovi = new ArrayList<>();
+        for (String i : strings) {
+            stringovi.add(i);
+        }
+
+        String datum = stringovi.get(0);
+
+        int dan,mesec,godina;
+
+        dan = Integer.parseInt(String.valueOf(datum.charAt(0)))*10 + Integer.parseInt(String.valueOf(datum.charAt(1)));
+
+        mesec = Integer.parseInt(String.valueOf(datum.charAt(3)))*10 + Integer.parseInt(String.valueOf(datum.charAt(4)));
+
+        godina = Integer.parseInt(String.valueOf(datum.charAt(6)))*1000 + Integer.parseInt(String.valueOf(datum.charAt(7)))*100 + Integer.parseInt(String.valueOf(datum.charAt(8)))*10 + Integer.parseInt(String.valueOf(datum.charAt(9)));
+
+        pocetakDan = LocalDateTime.of(godina,mesec,dan,0,0);
+
+
+        String datum2 = stringovi.get(1);
+
+        int dan2,mesec2,godina2;
+
+        dan2 = Integer.parseInt(String.valueOf(datum2.charAt(0)))*10 + Integer.parseInt(String.valueOf(datum2.charAt(1)));
+
+        mesec2 = Integer.parseInt(String.valueOf(datum2.charAt(3)))*10 + Integer.parseInt(String.valueOf(datum2.charAt(4)));
+
+        godina2 = Integer.parseInt(String.valueOf(datum2.charAt(6)))*1000 + Integer.parseInt(String.valueOf(datum2.charAt(7)))*100 + Integer.parseInt(String.valueOf(datum2.charAt(8)))*10 + Integer.parseInt(String.valueOf(datum2.charAt(9)));
+
+        krajDan = LocalDateTime.of(godina2,mesec2,dan2,0,0);
+
+
+
+        if (stringovi.size() == 2){
+            List<Termin> obrisi = new ArrayList<>();
+            if(pocetakDan.getYear() == krajDan.getYear() && pocetakDan.getMonth() == krajDan.getMonth() && pocetakDan.getDayOfMonth()==krajDan.getDayOfMonth()){
+                for (Termin t : getRaspored()){
+                    if (t.getPocetak().getYear() == pocetakDan.getYear() && t.getPocetak().getMonth() == pocetakDan.getMonth() && t.getPocetak().getDayOfMonth() == pocetakDan.getDayOfMonth()){
+                        obrisi.add(t);
+                    }
+                }
+                getRaspored().removeAll(obrisi);
+            }
+
+            for (LocalDateTime datumm = pocetakDan; datumm.isBefore(krajDan); datumm = datumm.plusDays(1)){
+
+                for (Termin t : getRaspored()){
+                    if (t.getPocetak().getYear() == datumm.getYear() && t.getPocetak().getMonth() == datumm.getMonth() && t.getPocetak().getDayOfMonth() == datumm.getDayOfMonth()){
+                        obrisi.add(t);
+                    }
+                }
+
+            }
+            getRaspored().removeAll(obrisi);
+        } else if (stringovi.size() == 4) {
+            List<Termin> obrisi = new ArrayList<>();
+            pocetakSati = Integer.parseInt(stringovi.get(2));
+
+            krajSati = Integer.parseInt(stringovi.get(3));
+
+            if(pocetakDan.getYear() == krajDan.getYear() && pocetakDan.getMonth() == krajDan.getMonth() && pocetakDan.getDayOfMonth()==krajDan.getDayOfMonth()){
+                LocalDateTime krajj = LocalDateTime.of(pocetakDan.getYear(), pocetakDan.getMonth(), pocetakDan.getDayOfMonth(), krajSati, 0);
+                LocalDateTime pocetakk = LocalDateTime.of(pocetakDan.getYear(), pocetakDan.getMonth(), pocetakDan.getDayOfMonth(), pocetakSati, 0);
+                for (Termin t : getRaspored()){
+                    if (t.getPocetak().getYear() == pocetakDan.getYear() && t.getPocetak().getMonth() == pocetakDan.getMonth() && t.getPocetak().getDayOfMonth() == pocetakDan.getDayOfMonth() && t.getPocetak().getHour() == pocetakk.getHour() && t.getKraj().getHour() == krajj.getHour()){
+                        obrisi.add(t);
+                    }
+                }
+                getRaspored().removeAll(obrisi);
+            }
+
+            for (LocalDateTime datumm = pocetakDan; datumm.isBefore(krajDan); datumm = datumm.plusDays(1)) {
+                LocalDateTime krajj = LocalDateTime.of(datumm.getYear(), datumm.getMonth(), datumm.getDayOfMonth(), krajSati, 0);
+                LocalDateTime pocetakk = LocalDateTime.of(datumm.getYear(), datumm.getMonth(), datumm.getDayOfMonth(), pocetakSati, 0);
+
+                for(Termin t : getRaspored()){
+                    if (t.getPocetak().getYear() == pocetakk.getYear() && t.getPocetak().getMonth() == pocetakk.getMonth() && t.getPocetak().getDayOfMonth() == pocetakk.getDayOfMonth() && t.getPocetak().getHour() == pocetakk.getHour() && t.getKraj().getHour() == krajj.getHour()){
+                        obrisi.add(t);
+                    }
+                }
+            }
+            getRaspored().removeAll(obrisi);
+        } else if (stringovi.size() == 3) {
+            List<Termin> obrisi = new ArrayList<>();
+            dann = stringovi.get(2);
+
+            switch (dann){
+                case "ponedeljak":
+                    danEng = DayOfWeek.MONDAY;
+                    break;
+                case "utorak":
+                    danEng = DayOfWeek.TUESDAY;
+                    break;
+                case "sreda":
+                    danEng = DayOfWeek.WEDNESDAY;
+                    break;
+                case "cetvrtak":
+                    danEng = DayOfWeek.THURSDAY;
+                    break;
+                case "petak":
+                    danEng = DayOfWeek.FRIDAY;
+                    break;
+                case "subota":
+                    danEng = DayOfWeek.SATURDAY;
+                    break;
+                case "nedelja":
+                    danEng = DayOfWeek.SUNDAY;
+                    break;
+            }
+            if(pocetakDan.getYear() == krajDan.getYear() && pocetakDan.getMonth() == krajDan.getMonth() && pocetakDan.getDayOfMonth()==krajDan.getDayOfMonth()){
+                for (Termin t : getRaspored()){
+                    if (t.getPocetak().getYear() == pocetakDan.getYear() && t.getPocetak().getMonth() == pocetakDan.getMonth() && t.getPocetak().getDayOfMonth() == pocetakDan.getDayOfMonth() && t.getPocetak().getDayOfWeek() == danEng){
+                        obrisi.add(t);
+                    }
+                }
+                getRaspored().removeAll(obrisi);
+            }
+
+            for (LocalDateTime datumm = pocetakDan; datumm.isBefore(krajDan); datumm = datumm.plusDays(1)) {
+                LocalDateTime krajj = LocalDateTime.of(datumm.getYear(), datumm.getMonth(), datumm.getDayOfMonth(), 0, 0);
+                LocalDateTime pocetakk = LocalDateTime.of(datumm.getYear(), datumm.getMonth(), datumm.getDayOfMonth(), 0, 0);
+
+                if (datumm.getDayOfWeek() == danEng){
+                    for(Termin t : getRaspored()){
+                        if (t.getPocetak().getYear() == datumm.getYear() && t.getPocetak().getMonth() == datumm.getMonth() && t.getPocetak().getDayOfMonth() == datumm.getDayOfMonth()){
+                            obrisi.add(t);
+                        }
+                    }
+                }
+
+
+            }
+            getRaspored().removeAll(obrisi);
+        } else if (stringovi.size() == 5) {
+            List<Termin> obrisi = new ArrayList<>();
+            pocetakSati = Integer.parseInt(stringovi.get(2));
+
+            krajSati = Integer.parseInt(stringovi.get(3));
+
+            dann = stringovi.get(4);
+
+            switch (dann){
+                case "ponedeljak":
+                    danEng = DayOfWeek.MONDAY;
+                    break;
+                case "utorak":
+                    danEng = DayOfWeek.TUESDAY;
+                    break;
+                case "sreda":
+                    danEng = DayOfWeek.WEDNESDAY;
+                    break;
+                case "cetvrtak":
+                    danEng = DayOfWeek.THURSDAY;
+                    break;
+                case "petak":
+                    danEng = DayOfWeek.FRIDAY;
+                    break;
+                case "subota":
+                    danEng = DayOfWeek.SATURDAY;
+                    break;
+                case "nedelja":
+                    danEng = DayOfWeek.SUNDAY;
+                    break;
+            }
+
+            if(pocetakDan.getYear() == krajDan.getYear() && pocetakDan.getMonth() == krajDan.getMonth() && pocetakDan.getDayOfMonth()==krajDan.getDayOfMonth()){
+                LocalDateTime krajj = LocalDateTime.of(pocetakDan.getYear(), pocetakDan.getMonth(), pocetakDan.getDayOfMonth(), krajSati, 0);
+                LocalDateTime pocetakk = LocalDateTime.of(pocetakDan.getYear(), pocetakDan.getMonth(), pocetakDan.getDayOfMonth(), pocetakSati, 0);
+                for (Termin t : getRaspored()){
+                    if (t.getPocetak().getYear() == pocetakDan.getYear() && t.getPocetak().getMonth() == pocetakDan.getMonth() && t.getPocetak().getDayOfMonth() == pocetakDan.getDayOfMonth() && t.getPocetak().getHour() == pocetakk.getHour() && t.getKraj().getHour() == krajj.getHour() && t.getPocetak().getDayOfWeek() == danEng){
+                        obrisi.add(t);
+                    }
+                }
+                getRaspored().removeAll(obrisi);
+            }
+
+            for (LocalDateTime datumm = pocetakDan; datumm.isBefore(krajDan); datumm = datumm.plusDays(1)) {
+                LocalDateTime krajj = LocalDateTime.of(datumm.getYear(), datumm.getMonth(), datumm.getDayOfMonth(), krajSati, 0);
+                LocalDateTime pocetakk = LocalDateTime.of(datumm.getYear(), datumm.getMonth(), datumm.getDayOfMonth(), pocetakSati, 0);
+
+                if (datumm.getDayOfWeek() == danEng){
+                    for (Termin t : getRaspored()){
+                        if (t.getPocetak().getYear() == pocetakk.getYear() && t.getPocetak().getMonth() == pocetakk.getMonth() && t.getPocetak().getDayOfMonth() == pocetakk.getDayOfMonth() && t.getPocetak().getHour() == pocetakk.getHour() && t.getKraj().getHour() == krajj.getHour()){
+                            obrisi.add(t);
+                        }
+                    }
+                }
+            }
+                getRaspored().removeAll(obrisi);
+            }
+
+
+        return true;
     }
 
     @Override
--- src/main/java/main.java
+++ src/main/java/main.java
@@ -3,10 +3,32 @@ public class main {
 
         PeriodImplementacija p = new PeriodImplementacija();
 
-        p.dodajNoviTermin("s1","petak","13","15","01.10.2023","25.10.2023","Profesor:Igor Mijanovic","Projektor:DA","Tip:predavanje");
+        p.dodajNoviTermin("s1","nedelja","13","15","01.10.2023","25.10.2023","Profesor:Igor Mijanovic","Projektor:DA","Tip:predavanje");
 
-        System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+        p.dodajNoviTermin("s1","utorak","14","18","01.10.2023","25.10.2023","Profesor:Igor Mijanovic","Projektor:DA","Tip:predavanje");
 
-        p.dodajNoviTermin("raf2","ponedeljak","19","20","05.05.2023","10.09.2023","Tip:Vezbe","Predmet:DS");
+        p.dodajNoviTermin("s1","sreda","14","18","08.11.2023","08.11.2023");
+
+        //System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+
+        //p.dodajNoviTermin("raf2","nedelja","12","14","01.10.2023","01.10.2023","Tip:Vezbe","Predmet:DS");
+
+        System.out.println(p.getRaspored().size());
+        for (Termin t: p.getRaspored()){
+            System.out.println(t);
+        }
+        System.out.println("Posle brisanjaaaaaaaaaaaaa");
+        //p.brisanjeTermina("01.10.2023","17.10.2023");
+        //p.brisanjeTermina("01.10.2023","25.10.2023","13","15");
+        //p.brisanjeTermina("01.10.2023","25.10.2023","nedelja");
+        //p.brisanjeTermina("01.10.2023","25.10.2023","13","15","nedelja");
+        //p.brisanjeTermina("08.11.2023","08.11.2023","14","18");
+        //p.brisanjeTermina("08.11.2023","08.11.2023","sreda");
+        p.brisanjeTermina("08.11.2023","08.11.2023","14","18","sreda");
+
+        System.out.println(p.getRaspored().size());
+        for (Termin t: p.getRaspored()){
+            System.out.println(t);
+        }
     }
 }
